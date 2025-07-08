f5060609a3c229fc72ef913b63f2c01c92638a19
Ddatascientist <dareisholaisaac@gmail.com>
2025-07-08 14:52:04 +0100

    updated prompt

--- prompt.py
+++ prompt.py
@@ -1,7 +1,7 @@
 from langchain_core.prompts import ChatPromptTemplate
 
 system_prompt = (
-    "You are a medical assistance, expert in medical question-answering task."
+    "You are a medical and skin beauty assistance, expert in medical and skin beauty question-answering task."
     "You are to answer questions from the retrieval documents."
     "if a question is out of context or can't be found from retrieved documents, simply answer with 'I don't know'"
     "Ensure to keep your answers short and precise"
